cc49e04c260d8f2b069321c94a62720376d673dc
fury-r <rajeev.dessai11@gmail.com>
2023-11-06 19:01:44 +0530

    fix footer alignment

--- src/app/component/Footer.tsx
+++ src/app/component/Footer.tsx
@@ -7,7 +7,7 @@ const MainFooter = () => {
       <footer className="footer flex  mt-4">
         <Container className="p-5 flex justify-content-center">
           <Row xs={1} xl={1} className="flex justify-center">
-            <label className="footer font-sm ">
+            <label className="footer font-sm  text-center">
               Designed & built by Rajeev Dessai.
             </label>
             <label className="footer font-sm  text-center ">

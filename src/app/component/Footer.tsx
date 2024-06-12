1b68e4352355015199fb6d453f191b122ae182f7
fury-r <rajeev.dessai11@gmail.com>
2024-06-12 19:29:53 +0530

    🍱 file sharer project

--- src/app/component/Footer.tsx
+++ src/app/component/Footer.tsx
@@ -7,7 +7,7 @@ const MainFooter = () => {
       <footer className="footer flex  mt-4">
         <Container className="p-5 flex-row justify-content-center items-center justify-center">
           <div className="flex flex-col  justify-center  items-center">
-            <label className="footer font-sm w-fit">
+            <label className="footer font-sm w-fit text-center">
               Designed & built by Rajeev Dessai.
             </label>
             <label className="footer font-sm w-fit ">(v1.0.0)</label>
